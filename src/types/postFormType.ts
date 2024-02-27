9baaa58fdcf23627db9383028abd647680d1f0fa
Shah Solayman Sinha <md.sinha.p@gmail.com>
2024-02-27 16:34:36 +0600

    vercel deploy bug fixed

---
+++ src/types/postFormType.ts
@@ -0,0 +1,17 @@
+export type TReceiverPostFormType = {
+	name: string;
+	mobile: string;
+	bloodGroup: string;
+	location: string;
+	description: string;
+	isBloodReceive: boolean;
+};
+
+export type TDonorPostFormType = {
+	name: string;
+	mobile: string;
+	bloodGroup: string;
+	location: string;
+	description: string;
+	isAvailable: boolean;
+};
