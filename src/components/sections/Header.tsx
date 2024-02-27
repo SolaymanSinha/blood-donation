7cf1c4a675e7a957faface7b10452ac6a0169644
Shah Solayman Sinha <md.sinha.p@gmail.com>
2024-02-27 11:35:51 +0600

    header section position bug fixed

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export function HeaderSection() {
 	return (
-		<div className='h-[50rem] w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center'>
+		<div className='h-screen my-auto w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center'>
 			{/* Radial gradient for the container to give a faded look */}
 			<div className='absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
-			<div className='w-11/12 mx-auto'>
+			<div className='w-11/12 mx-auto my-auto'>
 				<p className='text-5xl text-center md:text-8xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8'>
 					রক্ত দান: জীবনদান
 				</p>
